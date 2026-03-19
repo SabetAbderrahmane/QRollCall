c7babd856236c002c56a2c404da5c533aa3b3f42
SabetAbderrahmane <93416710+SabetAbderrahmane@users.noreply.github.com>
2026-03-19 08:55:54 +0800

    Delete backend directory

--- backend/app/schemas/qr.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class QRTokenResponse(BaseModel):
-    event_id: int
-    qr_token: str  # The QR code image will be served as a response, but here we can store token or URL for the image.
-
-    class Config:
-        orm_mode = True  # Tells Pydantic to work with SQLAlchemy models if needed
